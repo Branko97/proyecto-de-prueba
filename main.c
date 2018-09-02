cdb8d4d1012039ca59328489177e6aa1cc2b8789
Your Name <brankoarandjelovic2@gmail.com>
2018-09-02 14:32:26 -0300

    ahora si

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 
 int main()
 {
-    printf("Aumento el dolar y todo va a costar el doble que cagada!\n");
+    printf("Aumento el dolar y todo va a costar el doble que cagada y pa colmo estoy desempleado y tengo dolares para vender jejeje!\n");
     return 0;
 }
