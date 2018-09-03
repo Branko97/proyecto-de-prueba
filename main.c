69d31114e6a568bc1d3237a61b6fd0ea8eddad24
Your Name <brankoarandjelovic2@gmail.com>
2018-09-03 10:42:38 -0300

    vijesimo quinto cambio

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 
 int main()
 {
-    printf("Aumento el dolar y todo va a costar el doble que cagada y pa colmo estoy desempleado y tengo dolares para vender jejeje!\n");
+    printf("Aumento el dolar y todo va a costar el doble que cagada y pa colmo estoy desempleado y tengo dolares para vender y todavia tengo que hacer el tabajo practico jejeje!\n");
     return 0;
 }
